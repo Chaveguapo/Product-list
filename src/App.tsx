b6782fe2cb534fa36c5e4a786ec60dfdc6b932d8
Chaveguapo <catakeas@gmail.com>
2024-09-09 15:53:58 +0200

    blueprint of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="container sm text-amber-500 text-6xl">Hello </div>
+      <div className="p-5 text-left sm:text-left min-[375px]:text-left max-[1440px]">
+        <h1 className="font-extrabold py-3">Dessert</h1>
+        <div>
+          <div>Image</div>
+          <button>Add to Cart</button>
+          <h3>Waffle</h3>
+          <h2 className="font-medium text-base">Waffle with Berries</h2>
+          <h2>$2.30</h2>
+          <div></div>
+        </div>
+      </div>
     </>
   );
 }
