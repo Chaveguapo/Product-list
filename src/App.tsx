d6d6a1a39de5aba620e2830863261bd24227f101
Chaveguapo <catakeas@gmail.com>
2024-09-09 17:50:36 +0200

    Update layout design

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import "./App.css";
+import waffle from "./assets/Images/image-waffle-mobile.jpg";
 
 function App() {
   return (
     <>
-      <div className="p-5 text-left sm:text-left min-[375px]:text-left max-[1440px]">
-        <h1 className="font-extrabold py-3">Dessert</h1>
+      <div className="container bg-Rose-300 p-5 text-left sm:text-left min-[375px]:text-left max-[1440px]">
+        <h1 className="font-bold pb-6 text-3xl text-Rose_900">Desserts</h1>
         <div>
-          <div>Image</div>
-          <button>Add to Cart</button>
-          <h3>Waffle</h3>
-          <h2 className="font-medium text-base">Waffle with Berries</h2>
+          <img
+            src={waffle}
+            className="rounded-lg"
+            alt="waffle_with_berries"
+          ></img>
+          <a></a>
+          <button className=" text-sm font-medium border-2 rounded-full px-7 py-2 border-Rose_300 ">
+            Add to Cart
+          </button>
+          <h3 className=" text-sm text-Rose_400">Waffle</h3>
+          <h2 className=" text-Rose_900 font-medium text-base/6">
+            Waffle with Berries
+          </h2>
           <h2>$2.30</h2>
           <div></div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      padding: "2rem",
+    },
+    extend: {
+      colors: {
+        Red: " hsl(14, 86%, 42%)",
+        Green: "hsl(159, 69%, 38%)",
+        Rose_50: "hsl(20, 50%, 98%)",
+        Rose_300: "hsl(14, 25%, 72%)",
+        Rose_400: "hsl(7, 20%, 60%)",
+        Rose_500: "hsl(12, 20%, 44%)",
+        Rose_900: "hsl(14, 65%, 9%)",
+      },
+    },
   },
   plugins: [],
 };
