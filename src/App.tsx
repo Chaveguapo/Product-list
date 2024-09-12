9714329e7344d59b52eed1fce671fdaf2a16997c
Chaveguapo <catakeas@gmail.com>
2024-09-12 17:34:42 +0200

    update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
 import "./App.css";
 import waffle from "./assets/Images/image-waffle-mobile.jpg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-Rose_50 p-5 text-left sm:text-left min-[375px]:text-center max-[1440px]">
+      <div className="min-h-screen bg-Rose_50 p-5 text-left ">
         <h1 className="font-bold pb-6 text-3xl text-Rose_900">Desserts</h1>
         <div>
-          <div className="relative flex flex-col items-center ">
+          <div className=" flex flex-col items-center ">
             <img
               src={waffle}
               className="rounded-lg static"
               alt="waffle_with_berries"
             ></img>
-            <button className="fixed z-90 bottom-20 p-8  text-sm font-medium text-Rose_900 border-2 rounded-full py-2 border-Rose_300 bg-white">
+            <button className="relative z-30 bottom-5 p-9 text-sm font-bold text-Rose_900 border-2 rounded-full py-2 border-Rose_300 bg-white">
               Add to Cart
             </button>
           </div>
-          <div className="pt-9">
+          <div className="pt-1">
             <h3 className="text-sm text-Rose_400">Waffle</h3>
-            <h2 className=" text-Rose_900 font-medium text-base/6">
-              Waffle with Berries
-            </h2>
+            <h2 className=" text-Rose_900 font-bold">Waffle with Berries</h2>
             <h2 className=" text-Red text-sm font font-bold">$2.30</h2>
           </div>
         </div>
