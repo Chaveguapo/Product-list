596c64f50d0fc0da227fa3b11f17b6014d45efd4
Chaveguapo <catakeas@gmail.com>
2024-09-13 11:53:13 +0200

    Setting mobile layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
 import "./App.css";
 import waffle from "./assets/Images/image-waffle-mobile.jpg";
-
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-Rose_50 p-5 text-left ">
+      <div className="min-h-screen bg-Rose_50 p-6 text-left ">
         <h1 className="font-bold pb-6 text-3xl text-Rose_900">Desserts</h1>
+
         <div>
-          <div className=" flex flex-col items-center ">
-            <img
-              src={waffle}
-              className="rounded-lg static"
-              alt="waffle_with_berries"
-            ></img>
-            <button className="relative z-30 bottom-5 p-9 text-sm font-bold text-Rose_900 border-2 rounded-full py-2 border-Rose_300 bg-white">
-              Add to Cart
-            </button>
-          </div>
-          <div className="pt-1">
-            <h3 className="text-sm text-Rose_400">Waffle</h3>
-            <h2 className=" text-Rose_900 font-bold">Waffle with Berries</h2>
-            <h2 className=" text-Red text-sm font font-bold">$2.30</h2>
-          </div>
+          <ul>
+            <li className=" flex flex-col items-center pt-4 ">
+              <img
+                src={waffle}
+                className="rounded-lg static"
+                alt="waffle_with_berries"
+              ></img>
+              <button
+                aria-label="Add to cart"
+                className="relative z-30 bottom-5 p-14 text-sm font-bold text-Rose_900900 border- rounded-full py-3 border-Rose_300 bg-white active:bg-Red  focus:outline-none focus:text-Rose_50 focus:bg-Red"
+              >
+                Add to Cart
+              </button>
+            </li>
+            <div className="p-2">
+              <h3 className="text-sm text-Rose_400">Waffle</h3>
+              <h2 className=" text-Rose_900 font-bold">Waffle with Berries</h2>
+              <h2 className=" text-Red text-sm font font-bold">$2.30</h2>
+            </div>
+          </ul>
         </div>
       </div>
     </>
