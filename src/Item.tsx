f9461e08b4d4b63f21891b6497c6102d4d67bdd7
Chaveguapo <catakeas@gmail.com>
2024-09-17 17:07:20 +0200

    update button

--- src/Item.tsx
+++ src/Item.tsx
@@ -6,15 +6,15 @@ import { faCartPlus, faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 function App() {
   const [itemCount, setItemCount] = useState(0);
 
-  const handleAddToCart = () => {
-    setItemCount(itemCount + 1);
-  };
+  // const handleAddToCart = () => {
+  //   setItemCount(itemCount + 1);
+  // };
 
-  const handleRemoveFromCart = () => {
-    if (itemCount > 0) {
-      setItemCount(itemCount - 1);
-    }
-  };
+  // const handleRemoveFromCart = () => {
+  //   if (itemCount > 0) {
+  //     setItemCount(itemCount - 1);
+  //   }
+  // };
   return (
     <>
       <div className="min-h-screen bg-Rose_50 p-6 text-left ">
@@ -29,18 +29,25 @@ function App() {
             >
               <img
                 src={waffle}
-                className={"rounded-lg static transition-all duration-300"}
+                className="rounded-lg"
                 alt="waffle_with_berries"
               ></img>
               <button
                 aria-label="Add to cart"
-                className={`relative z-30 border bottom-5 p-7 text-sm font-bold text-Rose-900 rounded-full py-3 ${
-                  itemCount ? "bg-Red text-white" : "bg-white text-Rose_900"
-                } focus:outline-none transition-all duration-300`}
-                // onClick={handleCartToggle}
+                className="relative z-30 border bottom-5 p-7 text-sm font-bold text-Rose-900 rounded-full py-3 
+              bg-white text-Rose_900"
               >
                 <FontAwesomeIcon icon={faCartPlus} className="px-3" />
                 Add to Cart
+              </button>
+
+              <button
+                aria-label="Count of items"
+                className=" flex justify-between relative z-30 rounded-full px-14 p-3 text-sm font-bold bg-Red text-white"
+              >
+                <FontAwesomeIcon icon={faMinus} />
+                <p>{itemCount}</p>
+                <FontAwesomeIcon icon={faPlus} />
               </button>
             </li>
             <div className="p-2">
